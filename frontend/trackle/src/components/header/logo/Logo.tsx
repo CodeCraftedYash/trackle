b4154aae1fc72a0b84f12fc78e5c91d85b3f4c28
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-06-23 00:30:53 +0530

    feat(header): add initial logo

---
+++ frontend/trackle/src/components/header/logo/Logo.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Logo:React.FC = () => {
+  return (
+    <div className='w-4 h-4 bg-red-500 p-4 box-border rounded-full'>
+        <div className='w-2 h-2 bg-yellow-300 p-2 box-border rounded-full'>
+            <h2>yash</h2>
+        </div>
+    </div>
+  )
+}
+export default Logo;
