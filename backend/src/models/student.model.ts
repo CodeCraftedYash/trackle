cafd4a8669e5a42af5d3afbd1b0ec14d7e5bb5ce
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-06-28 00:46:28 +0530

    fix(models): remove timestamps from this model as it is extending from user model which already has timestamps

--- backend/src/models/student.model.ts
+++ backend/src/models/student.model.ts
@@ -1,2 +1,9 @@
 import mongoose from 'mongoose';
 import { UserModel } from './User.model';
+import { type Student } from './types/types'
+
+const studentSchema = new mongoose.Schema<Student>({
+    class: { type: String, required: true },
+    fees: { type: Number, required: true },
+    homework: [{ type: String }],
+})
