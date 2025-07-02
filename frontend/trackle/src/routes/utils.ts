927d0af52e0bcdb22dfc2180811e4a8352322f70
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-07-03 00:16:12 +0530

    feat(routes): add a util file to pull together the components and import it in routes file

---
+++ frontend/trackle/src/routes/utils.ts
@@ -0,0 +1,7 @@
+import Login from '../pages/auth/login/Login'
+import LandingPage from '../pages/landingPage/LandingPage'
+import Admin from '../pages/dashboard/admin/Admin'
+import User from '../pages/dashboard/user/User'
+import RootLayout from '../layout/rootLayout/RootLayout'
+
+export {Login,LandingPage,Admin,User,RootLayout}
