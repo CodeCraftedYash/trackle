2bab83b6da28104c6e22c46828d9e7540aa7c1a5
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-07-04 00:21:48 +0530

    feat(landing page): add data for benefits for teachers section component and implement it

---
+++ frontend/trackle/src/components/landingPage/benefitsForTeachers/benefitContent.ts
@@ -0,0 +1,25 @@
+import type { benefitContentType } from "../../../types/benefitContentType";
+import img1 from '../../../assets/image1.webp';
+import img2 from '../../../assets/image2.webp';
+import img3 from '../../../assets/image3.webp';
+
+export const benefitContent:benefitContentType[] = [
+    {
+        src:img1,
+        alt:"Save Time",
+        heading:"Save Time",
+        body:"Automate administrative tasks, such as attendance tracking and grading, freeing up valuable time for lesson planning and student interaction.",
+    },
+    {
+        src:img2,
+        alt:"Increase Engagement",
+        heading:"Increase Engagement",
+        body:"Create dynamic and interactive lessons that captivate students' attention and foster a more engaging learning environment.",
+    },
+    {
+        src:img3,
+        alt:"Improve Outcomes",
+        heading:"Improve Outcomes",
+        body:"Gain insights into student performance and tailor your instruction to meet individual needs, leading to improved learning outcomes.",
+    },
+]
