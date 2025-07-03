b07a24db7f863f26a3d8c22ffc0b3a2c5398ff36
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-07-04 00:00:51 +0530

    feat(landingPage): add key features content data and implement in key features component

---
+++ frontend/trackle/src/components/landingPage/keyFeatures/keyFeaturesContents.ts
@@ -0,0 +1,7 @@
+import type { KeyFeaturesType } from "../../../types/kyeFeaturesType";
+
+export const keyFeaturesContent:KeyFeaturesType[] = [
+    {title:"Student Management",body:"Easily organize and manage your student roster, including contact information and individual progress."},
+    {title:"Interactive ",body:"create engaging and productive learning environment and collaborative activites"},
+    {title:"Performance Tracking",body:"Monitor student performance with detailed analytics and reports, allowing you to identify areas for improvement and tailor your instruction accordingly."},
+]
