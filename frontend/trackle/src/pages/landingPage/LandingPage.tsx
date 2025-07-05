b47eb780c0d66ccef0c9cd4c07e7e30f40d93ca1
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-07-05 23:56:43 +0530

    feat(landing page): add all landing page  components here

---
+++ frontend/trackle/src/pages/landingPage/LandingPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LandingPageLayout from '../../layout/landingPage/LandingPageLayout'
+
+const LandingPage:React.FC = () => {
+  return (
+    <div>
+      <LandingPageLayout />
+    </div>
+  )
+}
+
+export default LandingPage
