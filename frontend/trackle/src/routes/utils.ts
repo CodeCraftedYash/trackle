79b64f369d79d0f214b7d7e5caafdc7f88103c30
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-07-07 00:01:21 +0530

    feat(routes): add not found component

--- frontend/trackle/src/routes/utils.ts
+++ frontend/trackle/src/routes/utils.ts
@@ -3,5 +3,6 @@ import LandingPage from '../pages/landingPage/LandingPage'
 import Admin from '../pages/dashboard/admin/Admin'
 import User from '../pages/dashboard/user/User'
 import RootLayout from '../layout/rootLayout/RootLayout'
+import NotFound from '../pages/NotFound'
 
-export {Login,LandingPage,Admin,User,RootLayout}
+export {Login,LandingPage,Admin,User,RootLayout,NotFound}
