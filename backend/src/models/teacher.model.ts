85c37bc59c5efbc4752a5e0303a511db82873ece
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-07-17 17:08:03 +0530

    feat(model): add teacher model

---
+++ backend/src/models/teacher.model.ts
@@ -0,0 +1,12 @@
+import mongoose, { Mongoose } from "mongoose";
+import { TeacherModelType } from "./types/mongooseModelTypes.js";
+const teacherSchema = new mongoose.Schema<TeacherModelType>(
+  { 
+    name: { type: String, required: true, unique: true },
+    gender: { type: String, required: true, enum: ["male", "female", "others"] },
+    password: { type: String, required: true },
+    role: { type: String, required: true, enum: ["student", "teacher"] },
+    mobileNumber: { type: String, required: true },
+    picture: { type: String },});
+
+export const TeacherModel = mongoose.model<TeacherModelType>("Teacher", teacherSchema);
