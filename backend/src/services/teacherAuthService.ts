e3cdcb268163bf6c31057320179c290a62a49f43
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-07-17 17:16:03 +0530

    feat(services): add teacher auth service

---
+++ backend/src/services/teacherAuthService.ts
@@ -0,0 +1,34 @@
+import { hashPassword,comparePassword } from "../utils/bcrypt.js";
+import { TeacherModel } from "../models/teacher.model.js";
+import { type TeacherType } from "../models/types/types.js";
+import jwt from 'jsonwebtoken';
+export async function registerTeacherService(data:Omit<TeacherType,'role' >){
+    const { name, gender, password, mobileNumber} = data;
+    const hashedPassword = await hashPassword(password);
+    const newTeacher = await TeacherModel.create({
+        name,
+        gender,
+        mobileNumber,
+        password: hashedPassword,
+        role: "teacher",
+        picture:"hfakdsafk"
+    })
+    return newTeacher;
+}
+
+export async function loginTeacherService(data:{name: string, password: string}){
+    const { name, password } = data;
+    const teacher = await TeacherModel.findOne({ name });
+    if( !teacher ) {
+        throw new Error("Teacher not found");
+    }
+    const isMatch = await ( comparePassword( password, teacher.password ));
+    if( !isMatch ){
+        throw new Error("Invalid password");
+    }
+    if (!process.env.JWT_SECRET) {
+        throw new Error("JWT_SECRET is not set in environment variables");
+    }
+    const token = jwt.sign({ id: teacher._id, role: teacher.role }, process.env.JWT_SECRET);
+    return {teacher,token};
+}
