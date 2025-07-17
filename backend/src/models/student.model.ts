5ee3f748cf7ee39e183780f3dcc06ade52ca43f0
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-07-17 17:03:30 +0530

    fix(model): fix typescript type issues

--- backend/src/models/student.model.ts
+++ backend/src/models/student.model.ts
@@ -1,9 +1,17 @@
 import mongoose from 'mongoose';
-import { UserModel } from './User.model';
-import { type Student } from './types/types'
+import { type StudentModelType } from './types/mongooseModelTypes.js';
 
-const studentSchema = new mongoose.Schema<Student>({
-    class: { type: String, required: true },
-    fees: { type: Number, required: true },
-    homework: [{ type: String }],
+const studentSchema = new mongoose.Schema<StudentModelType>({
+    name: { type: String, required: true, unique: true },
+    gender: { type: String, required: true, enum: ["male", "female", "others"] },
+    password: { type: String, required: true },
+    role: { type: String, required: true, enum: ["student", "teacher"] },
+    mobileNumber: { type: String, required: true },
+    picture: { type: String },
+    studyingClass: { type: String, required: true },
+    fees: { type: Number, default: 0 },
+    isFeesPaid: { type: Boolean, default: false },
+    homework : { type: [String], default: []  },
 })
+
+export const StudentModel = mongoose.model('student',studentSchema)
