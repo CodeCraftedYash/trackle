de91225a9c6a3939a0dacd6ee52ff03b52a7f124
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-07-24 23:54:11 +0530

    fix(service): fix the service to delete student by id instead of name

--- backend/src/services/studentDeleteService.ts
+++ backend/src/services/studentDeleteService.ts
@@ -1,6 +1,6 @@
 import { StudentModel } from "../models/student.model.js";
 
-export const deleteStudentByName = async (name: string) => {
-  const deleted = await StudentModel.findOneAndDelete({ name });
+export const deleteStudentById= async (id: string) => {
+  const deleted = await StudentModel.findOneAndDelete({ id });
   return deleted; 
 };
