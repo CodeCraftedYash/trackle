ed6e99d6920cec68a1d10b9ffc104fd506cfe3ba
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-08-12 12:29:16 +0530

    fix(type): change class to standard and picture type from blob to file

--- backend/src/models/types/types.ts
+++ backend/src/models/types/types.ts
@@ -1,14 +1,15 @@
 export interface TeacherType {
+  _id: string;
   name: string;
   gender: string;
   password: string;
   role: 'student' | 'teacher';
   mobileNumber: string;
-  picture: Blob | string;
+  picture: File | string;
 }
 
 export interface StudentType extends TeacherType {
-    studyingClass: string;
+    standard: string;
     fees: number;
     isFeesPaid: boolean;
     homework: string[];
