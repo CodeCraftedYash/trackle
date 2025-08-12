9a4d7be103278a187b3021859f1f0bc806f802fc
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-08-12 13:14:20 +0530

    feat(service): make the update password to be hashed using bcrypt hashPasswordUtility and add implement object cleaner utility to clear empty object keys

--- backend/src/services/studentUpdateService.ts
+++ backend/src/services/studentUpdateService.ts
@@ -1,23 +1,33 @@
-// services/studentUpdateService.ts
 import { StudentModel } from '../models/student.model.js';
 import { type StudentType } from '../models/types/types.js';
+import { Types } from 'mongoose';
+import { hashPassword } from '../utils/bcrypt.js';
+import { objectCleaner } from '../utils/objectCleaner.js';
 
 export const updateStudent= async (updatedData: Partial<StudentType>) => {
-  const { name } = updatedData;
-
-  if (!name) {
-    throw new Error("Student name is required to update.");
+  const { _id, password, ...rest } = updatedData;
+  
+  let updateData:Partial<StudentType> ={...rest};
+  if (!_id) {
+    throw new Error("Student ID is required to update.");
   }
 
-  const existingStudent = await StudentModel.findOne({ name });
+  updateData = objectCleaner<StudentType>(rest);
+
+  if (password) {
+    const hashedPassword = await hashPassword(password);
+    updateData.password = hashedPassword;
+  }
+
+  const existingStudent = await StudentModel.findOne({ _id: new Types.ObjectId(_id) });
 
   if (!existingStudent) {
     throw new Error("Student not found.");
   }
 
   const updatedStudent = await StudentModel.findOneAndUpdate(
-    { name },
-    { $set: updatedData },
+    { _id },
+    { $set: updateData },
     { new: true }
   );
 
