7a24ba2e2d678c2d84149d0b51eb146db80482dd
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-08-12 13:05:50 +0530

    feat(service): add feature to delete image from cloudinary when student is deleted

--- backend/src/services/studentDeleteService.ts
+++ backend/src/services/studentDeleteService.ts
@@ -1,6 +1,24 @@
 import { StudentModel } from "../models/student.model.js";
+import { v2 as cloudinary} from "cloudinary";
 
-export const deleteStudentById= async (id: string) => {
-  const deleted = await StudentModel.findOneAndDelete({ id });
+export const deleteStudentById= async (_id: string) => {
+  const student = await StudentModel.findById(_id);
+  if (!student) {
+    throw new Error(`Student with ID ${_id} not found`);
+  }
+  if (student.picture_Id) {
+    try {
+      await cloudinary.uploader.destroy(student.picture_Id);
+    }
+    catch (error : any ) {
+      throw new Error(`Failed to delete picture from cloudinary: ${error.message}`);
+    }
+  }
+
+  const deleted = await StudentModel.findByIdAndDelete(_id);
+  
+  if (!deleted) {
+    throw new Error(`Failed to delete student with ID ${_id}`);
+  }
   return deleted; 
 };
