5af92a37c1882dcc5b10761e1a3c901e48a87c19
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-08-12 12:59:15 +0530

    feat(routes): implement multer middleware

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import { registerStudentController, loginStudentController } from '../controllers/auth/studentAuthController.js';
 import { registerTeacherController, loginTeacherController } from '../controllers/auth/teacherAuthController.js';
+import { upload } from '../middlewares/multer.middleware.js';
 
 const router = express.Router();
 
-router.post('/student/register', registerStudentController);
+router.post('/student/register',upload.single('picture'), registerStudentController);
 router.post('/student/login', loginStudentController);
-router.post('/teacher/register', registerTeacherController);
+router.post('/teacher/register',upload.single('picture'), registerTeacherController);
 router.post('/teacher/login', loginTeacherController);
 
 export default router;
