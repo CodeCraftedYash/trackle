0de77745307ab5b84ba549094feb83a24e9f2341
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-08-12 13:24:06 +0530

    chore(app): remove needless codes and logs

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,25 @@
 import express from 'express';
 import cors from 'cors';
-
+import allRouter from './routes/index.js';
 const app = express();
-
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
 app.use(express.json());
 
-app.get('/',( req, res )=>{
-    res.send('its working')
-})
+app.use('/api', allRouter);
 
+/* const createAdmin = async () => {
+    const data ={
+        name:"Yash Mishra", gender:"male", password:"yashtesting", mobileNumber:"6299700545",picture:"rkfasdj"
+    }
+    try {
+        await registerTeacherService(data)
+        console.log("Admin created successfully");
+    } catch( error ) {
+        console.log(error);
+    }
+}
+createAdmin(); */
 export default app;
