d166de329710129ba6c76d9f9dd92452bcd3629e
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-08-12 13:00:41 +0530

    feat(routes): implement multer middleware

--- backend/src/routes/student.routes.ts
+++ backend/src/routes/student.routes.ts
@@ -1,11 +1,12 @@
-import { getStudents, updateStudentByName, deleteStudent } from "../controllers/student/studentController.js";
+import { getStudents, updateStudentByID, deleteStudent } from "../controllers/student/studentController.js";
 import express from 'express';
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router = express.Router();
 
 router.get('/getAllStudents', getStudents);
-router.post('/updateStudent', updateStudentByName);
-router.post('/deleteStudent', deleteStudent)
+router.post('/updateStudent',upload.single('picture'), updateStudentByID);
+router.post('/deleteStudent', deleteStudent);
 
 
 export default router;
