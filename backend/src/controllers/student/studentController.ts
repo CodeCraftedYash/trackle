d5eefa877296ddda9a9fee84153d1ffd06d875fe
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-08-12 12:26:06 +0530

    feat(controllers): add feature to update image and upload to cloudinary

--- backend/src/controllers/student/studentController.ts
+++ backend/src/controllers/student/studentController.ts
@@ -2,6 +2,9 @@ import { Request, Response } from 'express';
 import * as studentService from '../../services/studentsGetService.js';
 import { updateStudent } from '../../services/studentUpdateService.js';
 import { deleteStudentById } from '../../services/studentDeleteService.js';
+import { uploadOnCloudinary } from '../../services/cloudinary.service.js';
+import { objectCleaner } from '../../utils/objectCleaner.js';
+import { StudentType } from '../../models/types/types.js';
 
 
 export const getStudents = async (req: Request, res: Response) => {
@@ -10,9 +13,31 @@ export const getStudents = async (req: Request, res: Response) => {
 };
 
 
-export const updateStudentByName = async (req: Request, res: Response) => {
+export const updateStudentByID = async (req: Request, res: Response) => {
   try {
-    const updatedStudent = await updateStudent(req.body);
+    const { body, file, params } = req;
+    const studentId = body._id || params.id;
+    if (!studentId) {
+    res.status(400).json({ error: 'Student ID is required for update' });
+    return;
+    }
+
+    let updatedStudentData = objectCleaner<StudentType>(body) as Partial<StudentType> & { _id: string };
+
+    if (file) {
+      try {
+         const cloudinaryResponse = await uploadOnCloudinary(file.path);
+         if (cloudinaryResponse) {
+         updatedStudentData.picture = cloudinaryResponse.secure_url
+      }
+      }
+      catch (error : any) {
+  
+      }}
+
+    updatedStudentData._id = studentId;
+
+    const updatedStudent = await updateStudent(updatedStudentData);
     res.status(200).json(updatedStudent);
   } catch (err: any) {
     res.status(400).json({ error: err.message });
@@ -23,14 +48,13 @@ export const deleteStudent = async (
   req: Request,
   res: Response
 ): Promise<void> => {
-  const { id } = req.body;
-
-  if (!id){
+  const { _id } = req.body;
+  if (!_id){
     res.status(400).json({ error: 'Student id is required' });
     return;
   } 
 
-  const result = await deleteStudentById(id);
+  const result = await deleteStudentById(_id);
 
   if (!result) {
     res.status(404).json({ message: 'Student not found' });
