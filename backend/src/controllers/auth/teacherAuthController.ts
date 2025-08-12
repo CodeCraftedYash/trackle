b301bdbf5d37c16fc26d102aef64bb3914b083bb
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-08-12 12:24:56 +0530

    feat(controller): add feature to upload image to cloudinary

--- backend/src/controllers/auth/teacherAuthController.ts
+++ backend/src/controllers/auth/teacherAuthController.ts
@@ -1,36 +1,36 @@
 import { Request, Response } from 'express';
 import { loginTeacherService, registerTeacherService } from '../../services/teacherAuthService.js';
+import { uploadOnCloudinary } from '../../services/cloudinary.service.js';
 
 export async function registerTeacherController( req:Request, res:Response ){
     try {
-        const teacher = await registerTeacherService(req.body);
-        res.status(201).json({ 
-            _id: teacher._id,
-            name: teacher.name,
-            gender: teacher.gender,
-            role: teacher.role,
-            mobileNumber: teacher.mobileNumber,
-        });
-    } catch (error) {
-        if(error instanceof Error) {
-              res.status(400).json({ message: error.message });
+            const { body, file} = req;
+            const teacherData = { ...body };
+            if (file) {
+                  try {
+                     const cloudinaryResponse = await uploadOnCloudinary(file.path);
+                     if (cloudinaryResponse) {
+                     teacherData.picture = cloudinaryResponse.secure_url;
+                     teacherData.picture_Id = cloudinaryResponse.public_id;
+                  }
+                  }
+                  catch (error : any) {
+                    return res.status(500).json({ message: "Image upload failed at register controller" });
+                  }}
+            const teacher = await registerTeacherService(teacherData);
+            res.status(201).json(teacher);
+        } catch (error:any) {
+            if(error instanceof Error) {
+                  res.status(400).json({ message: error.message });
+            }
+            else res.status(500).json({ message: 'Something went wrong' });
         }
-        else res.status(400).json({ message: 'Something went wrong' });
-    }
 }
 
 export async function loginTeacherController( req:Request, res:Response ){
     try {
         const {teacher,token} = await loginTeacherService(req.body);
-        res.status(201).json({ 
-            _id: teacher._id,
-            name: teacher.name,
-            gender: teacher.gender,
-            role: teacher.role,
-            mobileNumber: teacher.mobileNumber,
-            message: 'Login successful',
-            token
-        });
+        res.status(201).json({teacher,token});
     } catch (error) {
         if(error instanceof Error) {
               res.status(400).json({ message: error.message });
