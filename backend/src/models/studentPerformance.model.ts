611b9f56c0475b7ef60ff42f0ea305d0ab2bf22e
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-08-14 16:21:54 +0530

    feat(models): add student performance model

---
+++ backend/src/models/studentPerformance.model.ts
@@ -0,0 +1,36 @@
+import mongoose, { Schema } from "mongoose";
+import type { StudentPerformanceType } from "./types/studentPerformanceModelType.js";
+const performanceSchema = new Schema<StudentPerformanceType>({
+    student_Id: {
+        type: Schema.Types.ObjectId,
+        ref: 'Student',
+        required: true,
+    },
+    year: {
+        type: Number,
+        required: true,
+    },
+    month: {
+        type: Number,
+        required: true,
+    },
+    dailyScore: {
+        type: [Number],
+        min: 0,
+        max: 3,
+        validate: {
+            validator: function (scores: number[]) {
+                return scores.every(score => score >= 0 && score <= 3);
+            },
+            message: 'Each score must be between 0 and 3.',
+        },
+        required: true,
+    },
+    monthlyPerformance: {
+        type: Number,
+    },
+})
+
+const StudentPerformance = mongoose.model('StudentPerformance', performanceSchema);
+
+export default StudentPerformance;
