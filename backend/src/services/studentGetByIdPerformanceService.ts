efe780e67f65ae1d2342348f8f7f8ceaec60c35f
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-08-19 16:57:00 +0530

    feat(service): add a service to get all the performance data of the current month for one student

---
+++ backend/src/services/studentGetByIdPerformanceService.ts
@@ -0,0 +1,19 @@
+import StudentPerformanceModel from "../models/studentPerformance.model.js";
+import { StudentPerformanceType } from "../models/types/studentPerformanceModelType.js";
+
+
+export const studentGetByIdPerformanceService = async(data:Omit<StudentPerformanceType,"dailyScore">) => {
+    const {student_Id,year,month} = data;
+    if (!student_Id || !year || !month) {
+    throw new Error("Missing required fields");
+  }
+  try{
+    const performanceResponse = await StudentPerformanceModel.find({
+        student_Id,year,month},
+    );
+    return performanceResponse
+  } catch(err){
+    throw new Error("Could not get performance in db");
+  }
+  
+} 
