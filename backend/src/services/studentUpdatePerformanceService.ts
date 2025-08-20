4911514d273b5031539963d1029ca50760fb0689
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-08-20 17:01:26 +0530

    fix(services): fix student performance update service to put score value at today's date as index in dailyScore  instead of just pushing it

--- backend/src/services/studentUpdatePerformanceService.ts
+++ backend/src/services/studentUpdatePerformanceService.ts
@@ -1,21 +1,35 @@
 import StudentPerformanceModel from "../models/studentPerformance.model.js";
 import { StudentPerformanceType } from "../models/types/studentPerformanceModelType.js";
 
-
 export const studentUpdatePerformanceService = async(data:Omit<StudentPerformanceType,"dailyScore">,score:number) => {
-    const {student_Id,year,month} = data;
-    if (!student_Id || !year || !month) {
+  const {student_Id,year,month} = data;
+  if (!student_Id || !year || !month) {
     throw new Error("Missing required fields");
-  }
-  try{
-    const performanceResponse = await StudentPerformanceModel.findOneAndUpdate({
-        student_Id,year,month},
-        {$push:{dailyScore:score}},
-        {new:true,upsert:true}    
-    );
-    return performanceResponse
-  } catch(err){
-    throw new Error("Could not create performance in db");
+  } 
+  const today = new Date().getDate()-1;
+   try {
+    let performance = await StudentPerformanceModel.findOne({
+      student_Id,
+      year,
+      month,
+    });
+
+    if (!performance) {
+
+      performance = new StudentPerformanceModel({
+        student_Id,
+        year,
+        month,
+        dailyScore: Array(31).fill(0),
+      });
+    }
+
+    performance.dailyScore[today] = score;
+
+    const updatedPerformance = await performance.save();
+    return updatedPerformance;
+  } catch (err) {
+    throw new Error("Could not update performance in db");
   }
   
 } 
