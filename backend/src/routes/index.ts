09cfc956d31864805c9ca82a31b188ea70618a1d
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-08-20 13:37:52 +0530

    feat(routes): add student performance route in the root route

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import authRoutes from './authRoutes.js'
 import studentRoutes from  './student.routes.js';
+import studentPerformanceRoutes from './studentPerformance.routes.js'
 
 const router = express.Router();
 
 router.use('/auth', authRoutes);
 router.use('/student', studentRoutes);
+router.use('/student/performance',studentPerformanceRoutes)
 
 export default router;
