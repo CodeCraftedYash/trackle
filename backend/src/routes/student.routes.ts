6938ffbe507f0610eda0b3b0c4279908688b9497
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-08-22 23:59:47 +0530

    feat(routes): add and implement verifyToken and verifyRole middlewares

--- backend/src/routes/student.routes.ts
+++ backend/src/routes/student.routes.ts
@@ -1,13 +1,15 @@
 import { getStudents, updateStudentByID, deleteStudent,assignHWStudentController, deleteAllStudentController } from "../controllers/student/studentController.js";
 import express from 'express';
 import { upload } from "../middlewares/multer.middleware.js";
+import { verifyToken } from "../middlewares/verifyJWT.middleware.js";
+import { verifyRole } from "../middlewares/verifyRole.middleware.js";
 
 const router = express.Router();
 
-router.get('/getAllStudents', getStudents);
-router.post('/updateStudent',upload.single('picture'), updateStudentByID);
-router.post('/deleteStudent', deleteStudent);
-router.post('/assignHW', assignHWStudentController);
-router.post('/deleteAllStudent',deleteAllStudentController);
+router.get('/getAllStudents', verifyToken,verifyRole(['teacher']), getStudents);
+router.post('/updateStudent',upload.single('picture'),verifyToken,verifyRole(['teacher']), updateStudentByID);
+router.post('/deleteStudent',verifyToken,verifyRole(['teacher']), deleteStudent);
+router.post('/assignHW' , verifyToken,verifyRole(['teacher']), assignHWStudentController);
+router.post('/deleteAllStudent',verifyToken,verifyRole(['teacher']),deleteAllStudentController);
 
 export default router;
