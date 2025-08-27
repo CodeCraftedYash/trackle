d9063b5e816768a47f13717120d7a8c6fec13ff0
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-08-27 22:29:46 +0530

    fix(service): implement getDaysInMonth utility to make score array dynamic based on days in month

--- backend/src/services/studentCreatePerformanceService.ts
+++ backend/src/services/studentCreatePerformanceService.ts
@@ -1,9 +1,10 @@
 import StudentPerformanceModel from "../models/studentPerformance.model.js";
+import { getDaysInMonth } from "../utils/getDaysInMonth.js";
 
 export const  studentCreatePerformanceService = async(
     student_Id:string,
     year:number = new Date().getFullYear(),
-    month: number = new Date().getMonth() + 1
+    month: number = new Date().getMonth() 
 ) => {
    if(!student_Id){
      throw new Error("Student Id does not exists");    
@@ -18,12 +19,12 @@ export const  studentCreatePerformanceService = async(
   if (existingPerformance) {
     return existingPerformance;
   }
-
+  const daysInMonth = getDaysInMonth(year,month);
    const studentPerformance = await StudentPerformanceModel.create({
      student_Id,
      year,
      month,
-     dailyScore:[]
+     dailyScore:Array(daysInMonth).fill(0)
    })
    return studentPerformance;
 }
