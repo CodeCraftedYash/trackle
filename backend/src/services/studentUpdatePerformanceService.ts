0e0e29f1e3b3e71f1caa2d11f261276b42845b98
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-08-27 22:31:37 +0530

    fix(service): implement getDaysInMonth utility to make score array dynamic also set up condition to check the score's range

--- backend/src/services/studentUpdatePerformanceService.ts
+++ backend/src/services/studentUpdatePerformanceService.ts
@@ -1,35 +1,38 @@
 import StudentPerformanceModel from "../models/studentPerformance.model.js";
 import { StudentPerformanceType } from "../models/types/studentPerformanceModelType.js";
+import { getDaysInMonth } from "../utils/getDaysInMonth.js";
 
 export const studentUpdatePerformanceService = async(data:Omit<StudentPerformanceType,"dailyScore">,score:number) => {
   const {student_Id,year,month} = data;
   if (!student_Id || !year || !month) {
     throw new Error("Missing required fields");
   } 
-  const today = new Date().getDate()-1;
-   try {
-    let performance = await StudentPerformanceModel.findOne({
-      student_Id,
-      year,
-      month,
-    });
 
-    if (!performance) {
-
-      performance = new StudentPerformanceModel({
-        student_Id,
-        year,
-        month,
-        dailyScore: Array(31).fill(0),
-      });
-    }
-
-    performance.dailyScore[today] = score;
-
-    const updatedPerformance = await performance.save();
-    return updatedPerformance;
-  } catch (err) {
-    throw new Error("Could not update performance in db");
+  if( score < 0 || score > 3){
+    throw new Error("Score must be between 0 and 3");
   }
-  
+
+  const today = new Date().getDate()-1;
+
+  let performance = await StudentPerformanceModel.findOne({
+    student_Id,
+    year,
+    month,
+  });
+
+  if (!performance) {
+    const daysInMonth = getDaysInMonth(year,month);
+    performance = new StudentPerformanceModel({
+    student_Id,
+    year,
+    month,
+    dailyScore: Array(daysInMonth).fill(0),
+    });
+  }
+  if(today > performance.dailyScore.length){
+    throw new Error("Invalid day index for this month");
+  }
+  performance.dailyScore[today] = score;
+
+  return await performance.save();
 } 
