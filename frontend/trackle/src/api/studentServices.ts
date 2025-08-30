e8d14c75e0a8fc8dd528b18b0f97e3df35a6d963
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-08-30 22:26:29 +0530

    feat(api): implement loading spinner

--- frontend/trackle/src/api/studentServices.ts
+++ frontend/trackle/src/api/studentServices.ts
@@ -1,18 +1,78 @@
-import axios from 'axios';
+import { useLoadingStore } from '../store/loadingStore';
 import { type StudentType } from '../types/studentType';
-import { type AddStudentData } from '../types/AddStudentDataType';
 import axiosInstance from './axios';
 
-const API_URL = '/auth/student/register';
+const updateStudentURL = '/student/updateStudent';
+const deleteStudentURL = '/student/deleteStudent';
+const getStudentsURL = '/student/getAllStudents';
+const addStudentURL = '/auth/student/register';
+const deleteAllStudentURL = '/student/deleteAllStudent';
+const assignHWURL = '/student/assignHW';
+const getAllStudentPerformanceURL = '/student/performance/getAllStudentPerformance';
+const updateStudentPerformanceURL = '/student/performance/updateStudentPerformance';
 
-export const getStudents = async (): Promise<StudentType[]> =>
-  (await axios.get(API_URL)).data;
+const { setIsLoading } = useLoadingStore.getState();
 
-export const addStudent = async (student: AddStudentData) =>
-  axiosInstance.post(API_URL, student);
+export const getStudents = async (): Promise<StudentType[]> => {
+  setIsLoading(true);
+  const res = await axiosInstance.get(getStudentsURL)
+  setIsLoading(false);
+  return res.data;
+}
 
-export const deleteStudent = async (id: string) =>
-  axios.delete(`${API_URL}/${id}`);
+export const addStudent = async (student: FormData) =>{
+  setIsLoading(true);
+  const res = await axiosInstance.post(addStudentURL, student);
+  setIsLoading(false);
+  return res.data;
+}
 
-export const updateStudent = async (student: StudentType) =>
-  axios.put(`${API_URL}/${student.id}`, student);
+export const deleteStudent = async (_id: string) =>{
+  setIsLoading(true);
+  const res = await axiosInstance.post(deleteStudentURL,{_id});
+  setIsLoading(false);
+  return res.data;
+}
+
+export const updateStudent = async (updatedStudent: FormData) =>{
+  setIsLoading(true);
+  const res = await axiosInstance.post(updateStudentURL, updatedStudent , {headers: { "Content-Type": "multipart/form-data" }});
+  setIsLoading(false);
+  return res.data;
+}
+
+export const deleteAllStudent = async (_Id:string,password:string) => {
+  setIsLoading(true);
+  const res = await axiosInstance.post(deleteAllStudentURL,{_Id,password});
+  setIsLoading(false);
+  return res.data;
+}
+
+export const assignHW = async (student_Id:string,HW:string) => {
+  setIsLoading(true);
+  const res = await axiosInstance.post(assignHWURL,{student_Id,HW});
+  setIsLoading(false);
+  return res.data;
+}
+
+export const getAllStudentPerformance = async () => {
+  setIsLoading(true);
+  const res = await axiosInstance.get(getAllStudentPerformanceURL);
+  setIsLoading(false);
+  return res.data;
+}
+
+export const getStudentPerformance = async (student_Id:string) => {
+  setIsLoading(true);
+  const getStudentPerformanceURL = `/student/performance/getStudentPerformance/${student_Id}`;
+  const res = await axiosInstance.get(getStudentPerformanceURL);
+  setIsLoading(false);
+  return res.data;
+}
+
+export const updateStudentPerformance = async (student_Id:string,month:number,year:number,score:number) => {
+  setIsLoading(true);
+  const res = await axiosInstance.post(updateStudentPerformanceURL,{student_Id,year,month,score});
+  setIsLoading(false);
+  return res.data;
+}
