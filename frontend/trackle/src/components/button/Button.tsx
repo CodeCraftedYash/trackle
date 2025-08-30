6d8ae9986cf2f31b2ba328c663e61d540ec94a29
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-08-30 22:49:49 +0530

    refactor(button): adjust responsiveness of button componenet

--- frontend/trackle/src/components/button/Button.tsx
+++ frontend/trackle/src/components/button/Button.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import type { ButtonProps } from '../../types/buttonType'
 
-const Button:React.FC<ButtonProps> = ({ cb,children,variant }) => {
-  const baseClass = " bg-[var(--color-accent)]  hover:cursor-pointer hover:scale-105 lg:hover:shadow-lg shadow-white/20 rounded-lg border-2 border-[var(--color-border)] truncate transition-all duration-150 "
+const Button:React.FC<ButtonProps> = ({ cb,children,variant,customStyle }) => {
+  const baseClass =  `${customStyle} bg-[var(--color-accent)] w-fit basis-1/3 lg:basis-1/5 lg:flex-none flex-auto hover:cursor-pointer hover:scale-105 lg:hover:shadow-lg hover:bg-green-500 shadow-white/20 rounded-lg border-2 border-[var(--color-border)] truncate p-2 transition-all duration-150 flex flex-nowrap items-center justify-between `
   const buttonClassName = {
-    primary:'w-[8rem] lg:w-[9rem] text-[.8rem] px-2 py-1'+baseClass,
-    secondary:''+baseClass,
+    primary:`text-[.7rem] ${baseClass}`,
+    secondary:`${baseClass}`,
   }  
   return (
-   <button onClick={cb} className={buttonClassName[variant]}>{children}</button>
+   <button onClick={cb} className={buttonClassName[variant]}
+    style={{fontSize:variant == 'primary' ? "var(--font-size-base)":"var(--font-size-small)"}}
+  >
+    {children}
+  </button>
   )
 }
 
