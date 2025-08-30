7cabf7109c292e902b2786fe2707b150def57afd
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-08-30 22:26:03 +0530

    feat(api): implement loading spinner

--- frontend/trackle/src/api/loginService.ts
+++ frontend/trackle/src/api/loginService.ts
@@ -1,14 +1,18 @@
+import { useLoadingStore } from "../store/loadingStore";
 import axiosInstance from "./axios";
 
 
 export const loginService = async (name: string, password: string, role: string) => {
-
+    const {setIsLoading} = useLoadingStore.getState();
     const loginURL = `/auth/${role}/login`;
     try {
+        setIsLoading(true);
         const res = await axiosInstance.post(loginURL, { name, password, role });
+        setIsLoading(false);
         return res.data;
     }
     catch (error) {
+        setIsLoading(false);
         throw error;
     }
 }
