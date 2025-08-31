fe59c4de19a86cae5b14a26684c253b293448565
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-08-31 15:42:16 +0530

    feat(navbar): implement confirm dialog for logout clicks

--- frontend/trackle/src/components/header/navbar/Navbar.tsx
+++ frontend/trackle/src/components/header/navbar/Navbar.tsx
@@ -1,18 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { navItems } from './NavItems'
+import { Link, useNavigate, } from '@tanstack/react-router';
+import { useAuthStore } from '../../../store/authStore';
+import { ConfirmDialog } from '../../dialogBox/ConfirmDialog';
 
-const Navbar: React.FC = () => {
+type Prop = {
+    setOpen?: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+const Navbar: React.FC<Prop> = ({setOpen}) => {
+    const navigate = useNavigate();
+    const [showConfirm, setShowConfirm] = useState(false);
+    const name = useAuthStore(state => state.user?.name);
+    const role = useAuthStore(state => state.user?.role);
+    const logout = useAuthStore(state => state.logout);
+
+    const handleConfirm = () => {
+        logout();
+        setShowConfirm(false);
+        navigate({ to: '/login' });
+        setOpen?.(false);
+    }
+
+    const handleCancel = () => {
+        setShowConfirm(false);
+    }
+
+    const handleClick = () => {
+        if(role){
+            setShowConfirm(true);
+        }
+        else if(!role){
+            navigate({ to : '/login' });
+        }
+    }
   return (
-    <nav className='text-(--color-text) '>
-        <ul className='flex flex-nowrap items-center gap-4'>
+    <nav className='text-[var(--color-text)]'>
+        <ul className='flex flex-col lg:flex-row flex-nowrap items-center gap-2 '>
             {
                 navItems.map((item, index) => (
-                    <li key={index} className='hover:scale-105 box-border border-2 px-2 rounded-xl hover:bg-(--color-secondary) transition-all duration-150' style={{ fontSize: 'var(--font-size-base) ' }}>
+                    <li key={index} className='hover:scale-105 box-border border-2 px-2 rounded-xl hover:bg-[var(--color-secondary)] transition-all duration-150 w-14 md:w-16 lg:w-18'>
+                        <Link to={item.path} 
+                        onClick={ () => setOpen?.(false) }
+                        activeProps={{ className : 'text-green-500'}}
+                        >
                         {item.title}
+                        </Link>
                     </li>
                 ))
-            }
+            }   
+            {role &&<li className='hover:scale-105 box-border border-2 px-2 rounded-xl hover:bg-[var(--color-surface)] transition-all duration-150 w-14 md:w-15 lg:w-16'> <Link to={role === 'teacher' ? '/admin':`/user/${name}`} activeProps={{ className : 'text-green-500'}} onClick={ () => setOpen?.(false) }>{role == 'teacher' ? 'Admin': 'User' }</Link>
+            </li>}
+            <li>
+            <button onClick={handleClick} className=' h-full flex items-center justify-center gap-2 hover:scale-105 box-border border-2 px-2 pb-0.5 rounded-xl hover:bg-[var(--color-surface)] transition-all duration-150 hover:cursor-pointer'
+            style={{backgroundColor: role ? 'red' : 'green'}}>
+                {role ? 'Logout' : 'Login'}
+            </button>
+            </li>
         </ul>
+        {showConfirm && <div className='fixed top-0 left-0 w-full h-full flex items-center justify-center z-50'>
+            <ConfirmDialog onConfirm={handleConfirm} onCancel={handleCancel} message='You will be logged out'/>
+        </div>}
     </nav>
   )
 }
