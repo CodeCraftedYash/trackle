6be24dd9a9f28fc4f2f587eb2ceb417fc3034f24
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-09-04 03:53:01 +0530

    feat(App): add animation using motion library

--- frontend/trackle/src/App.tsx
+++ frontend/trackle/src/App.tsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { RouterProvider } from '@tanstack/react-router';
+import './styles/global.css'
+import { router } from './routes/routes';
+import { AnimatePresence, motion } from 'motion/react';
+import DialogBox from './components/dialogBox/DialogBox';
+import Loading from './components/loading/Loading';
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+          <AnimatePresence mode="wait">
+        <motion.div
+          key={location.pathname}
+          initial={{ opacity: 0,}}
+          animate={{ opacity: 1}}
+          exit={{ opacity: 0}}
+          transition={{ duration: .3, ease: 'easeInOut' }}
+        >
+          <RouterProvider router={router}/>
+          <DialogBox />
+          <Loading />
+        </motion.div>
+      </AnimatePresence>
+  
+)
 }
 
 export default App
