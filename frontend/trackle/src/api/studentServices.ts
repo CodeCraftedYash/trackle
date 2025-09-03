c5996ad91966ee2e344324a9b05c11b91c4f6fc8
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-09-03 13:49:11 +0530

    fix(api): fix infinite loading issue by puttin every api call block into try catch and finally and setting off loading in finally

--- frontend/trackle/src/api/studentServices.ts
+++ frontend/trackle/src/api/studentServices.ts
@@ -1,78 +1,140 @@
-import { useLoadingStore } from '../store/loadingStore';
-import { type StudentType } from '../types/studentType';
-import axiosInstance from './axios';
+import { useLoadingStore } from "../store/loadingStore";
+import { type StudentType } from "../types/studentType";
+import axiosInstance from "./axios";
 
-const updateStudentURL = '/student/updateStudent';
-const deleteStudentURL = '/student/deleteStudent';
-const getStudentsURL = '/student/getAllStudents';
-const addStudentURL = '/auth/student/register';
-const deleteAllStudentURL = '/student/deleteAllStudent';
-const assignHWURL = '/student/assignHW';
-const getAllStudentPerformanceURL = '/student/performance/getAllStudentPerformance';
-const updateStudentPerformanceURL = '/student/performance/updateStudentPerformance';
+const updateStudentURL = "/student/updateStudent";
+const deleteStudentURL = "/student/deleteStudent";
+const getStudentsURL = "/student/getAllStudents";
+const addStudentURL = "/auth/student/register";
+const deleteAllStudentURL = "/student/deleteAllStudent";
+const assignHWURL = "/student/assignHW";
+const getAllStudentPerformanceURL =
+  "/student/performance/getAllStudentPerformance";
+const updateStudentPerformanceURL =
+  "/student/performance/updateStudentPerformance";
 
 const { setIsLoading } = useLoadingStore.getState();
 
 export const getStudents = async (): Promise<StudentType[]> => {
-  setIsLoading(true);
-  const res = await axiosInstance.get(getStudentsURL)
-  setIsLoading(false);
-  return res.data;
-}
+  try {
+    setIsLoading(true);
+    const res = await axiosInstance.get(getStudentsURL);
+    return res.data;
+  } catch (error: any) {
+    throw error;
+  } finally {
+    setIsLoading(false);
+  }
+};
 
-export const addStudent = async (student: FormData) =>{
-  setIsLoading(true);
-  const res = await axiosInstance.post(addStudentURL, student);
-  setIsLoading(false);
-  return res.data;
-}
+export const addStudent = async (student: FormData) => {
+  try {
+    setIsLoading(true);
+    const res = await axiosInstance.post(addStudentURL, student);
+    return res.data;
+  } catch (error: any) {
+    throw error;
+  } finally {
+    setIsLoading(false);
+  }
+};
 
-export const deleteStudent = async (_id: string) =>{
-  setIsLoading(true);
-  const res = await axiosInstance.post(deleteStudentURL,{_id});
-  setIsLoading(false);
-  return res.data;
-}
+export const deleteStudent = async (_id: string) => {
+  try {
+    setIsLoading(true);
+    const res = await axiosInstance.post(deleteStudentURL, { _id });
+    return res.data;
+  } catch (error: any) {
+    throw error;
+  } finally {
+    setIsLoading(false);
+  }
+};
 
-export const updateStudent = async (updatedStudent: FormData) =>{
-  setIsLoading(true);
-  const res = await axiosInstance.post(updateStudentURL, updatedStudent , {headers: { "Content-Type": "multipart/form-data" }});
-  setIsLoading(false);
-  return res.data;
-}
+export const updateStudent = async (updatedStudent: FormData) => {
+  try {
+    setIsLoading(true);
+    const res = await axiosInstance.post(updateStudentURL, updatedStudent, {
+      headers: { "Content-Type": "multipart/form-data" },
+    });
+    return res.data;
+  } catch (error: any) {
+    throw error;
+  } finally {
+    setIsLoading(false);
+  }
+};
 
-export const deleteAllStudent = async (_Id:string,password:string) => {
-  setIsLoading(true);
-  const res = await axiosInstance.post(deleteAllStudentURL,{_Id,password});
-  setIsLoading(false);
-  return res.data;
-}
+export const deleteAllStudent = async (_Id: string, password: string) => {
+  try {
+    setIsLoading(true);
+    const res = await axiosInstance.post(deleteAllStudentURL, {
+      _Id,
+      password,
+    });
+    return res.data;
+  } catch (error: any) {
+    throw error;
+  } finally {
+    setIsLoading(false);
+  }
+};
 
-export const assignHW = async (student_Id:string,HW:string) => {
-  setIsLoading(true);
-  const res = await axiosInstance.post(assignHWURL,{student_Id,HW});
-  setIsLoading(false);
-  return res.data;
-}
+export const assignHW = async (student_Id: string, HW: string) => {
+  try {
+    setIsLoading(true);
+    const res = await axiosInstance.post(assignHWURL, { student_Id, HW });
+    return res.data;
+  } catch (error: any) {
+    throw error;
+  } finally {
+    setIsLoading(false);
+  }
+};
 
 export const getAllStudentPerformance = async () => {
-  setIsLoading(true);
-  const res = await axiosInstance.get(getAllStudentPerformanceURL);
-  setIsLoading(false);
-  return res.data;
-}
+  try {
+    setIsLoading(true);
+    const res = await axiosInstance.get(getAllStudentPerformanceURL);
+    return res.data;
+  } catch (error: any) {
+    throw error;
+  } finally {
+    setIsLoading(false);
+  }
+};
 
-export const getStudentPerformance = async (student_Id:string) => {
-  setIsLoading(true);
-  const getStudentPerformanceURL = `/student/performance/getStudentPerformance/${student_Id}`;
-  const res = await axiosInstance.get(getStudentPerformanceURL);
-  setIsLoading(false);
-  return res.data;
-}
+export const getStudentPerformance = async (student_Id: string) => {
+  try {
+    setIsLoading(true);
+    const getStudentPerformanceURL = `/student/performance/getStudentPerformance/${student_Id}`;
+    const res = await axiosInstance.get(getStudentPerformanceURL);
+    return res.data;
+  } catch (error: any) {
+    throw error;
+  } finally {
+    setIsLoading(false);
+  }
+};
 
-export const updateStudentPerformance = async (student_Id:string,month:number,year:number,score:number) => {
-  setIsLoading(true);
-  const res = await axiosInstance.post(updateStudentPerformanceURL,{student_Id,year,month,score});
-  setIsLoading(false);
-  return res.data;
-}
+export const updateStudentPerformance = async (
+  student_Id: string,
+  month: number,
+  year: number,
+  score: number
+) => {
+  try {
+    setIsLoading(true);
+    const res = await axiosInstance.post(updateStudentPerformanceURL, {
+      student_Id,
+      year,
+      month,
+      score,
+    });
+    return res.data;
+  } catch (error: any) {
+    throw error;
+  } finally {
+    setIsLoading(false);
+  }
+};
