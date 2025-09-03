985ff70c7d6e0a2d56f69126b67ab7a4ce62b7d8
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-09-03 13:44:37 +0530

    fix(service): fix out of bounce issuse caused by mismatched indexing

--- backend/src/services/studentGetAllPerformanceService.ts
+++ backend/src/services/studentGetAllPerformanceService.ts
@@ -4,7 +4,7 @@ import StudentPerformance from "../models/studentPerformance.model.js"
 export const studentGetAllPerformanceService = async () =>{
   const now = new Date();
   const currentYear = now.getFullYear();
-  const currentMonth = now.getMonth() + 1;
+  const currentMonth = now.getMonth();
   try{
      const studentPerformanceData = await StudentPerformance.find({year:currentYear,month:currentMonth});
      return studentPerformanceData;
