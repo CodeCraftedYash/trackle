07ddc261a0ba17db07d8ecc2040706c905dd6ec4
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-09-04 02:24:52 +0530

    styel(layout): tweek style for better ui

--- frontend/trackle/src/layout/landingPage/LandingPageLayout.tsx
+++ frontend/trackle/src/layout/landingPage/LandingPageLayout.tsx
@@ -8,7 +8,7 @@ const LandingPageLayout:React.FC = () => {
   const token = useAuthStore(state => state.token);
   return (
     <div className='w-full min-h-[90dvh] bg-[var(--color-background)] text-[var(--color-text)] box-border py-4'>
-        <main className='w-[90%] lg:w-[65%]  min-h-20 mx-auto lg:mt-8'>
+        <main className='w-[90%] lg:w-[65%]  min-h-20 mx-auto lg:mt-8 flex flex-col gap-10 lg:gap-32'>
            <Hero />
            <KeyFeatures />
            <BenefitsForTeacher />
