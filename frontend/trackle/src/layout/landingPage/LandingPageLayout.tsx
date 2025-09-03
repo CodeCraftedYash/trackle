da25c71f4c1bc922c71b0565134e0874a2f92f07
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-09-03 16:18:05 +0530

    feat(layout): use authStore to conditionaly render a component if token is present

--- frontend/trackle/src/layout/landingPage/LandingPageLayout.tsx
+++ frontend/trackle/src/layout/landingPage/LandingPageLayout.tsx
@@ -3,14 +3,16 @@ import Hero from '../../components/landingPage/Hero'
 import KeyFeatures from '../../components/landingPage/keyFeatures/KeyFeatures'
 import BenefitsForTeacher from '../../components/landingPage/benefitsForTeachers/BenefitsForTeacher'
 import ReadyToTransform from '../../components/landingPage/ReadyToTransform/ReadyToTransform'
+import { useAuthStore } from '../../store/authStore'
 const LandingPageLayout:React.FC = () => {
+  const token = useAuthStore(state => state.token);
   return (
     <div className='w-full min-h-[90dvh] bg-[var(--color-background)] text-[var(--color-text)] box-border py-4'>
         <main className='w-[90%] lg:w-[65%]  min-h-20 mx-auto lg:mt-8'>
            <Hero />
            <KeyFeatures />
            <BenefitsForTeacher />
-           <ReadyToTransform />
+           {!token&&<ReadyToTransform />}
         </main>
     </div>
   )
