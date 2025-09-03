1e88623bfe3f35e7876456d8ca49df0f273316a5
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-09-03 16:32:28 +0530

    feat(pages): add admin page

---
+++ frontend/trackle/src/pages/dashboard/admin/Admin.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import AdminLayout from '../../../layout/Admin/AdminLayout'
+
+const Admin:React.FC = () => {
+  return (
+    <AdminLayout />
+  )
+}
+
+export default Admin
