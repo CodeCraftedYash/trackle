e19f9b0004d8c1c7abf117da47368bfbf1adab58
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-09-03 17:05:56 +0530

    refactor(store): refactor the code for better usability

--- frontend/trackle/src/store/dialogStore.ts
+++ frontend/trackle/src/store/dialogStore.ts
@@ -1,10 +1,18 @@
 import { create } from 'zustand';
 
-type DialogStoreType = {
-    isOpen: boolean;
-    setIsOpen: (isOpen: boolean) => void;
+interface DialogStateType {
+  isOpen: boolean;
+  message: string;
+  title: string;
+  openDialog: (msg: string, title?: string) => void;
+  closeDialog: () => void;
 }
-export const useDialogStore = create<DialogStoreType>((set) => ({
-    isOpen: false,
-    setIsOpen: (isOpen : boolean) => set({ isOpen})
-}))
+
+export const useDialogStore = create<DialogStateType>((set) => ({
+  isOpen: false,
+  message: '',
+  title: 'Notification!',
+  openDialog: (msg, title = 'Notification!') =>
+    set({ isOpen: true, message: msg, title }),
+  closeDialog: () => set({ isOpen: false }),
+}));
