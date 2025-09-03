d60ee5facdb81d5d902e6b7bb4ab903d50feed77
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-09-03 16:34:10 +0530

    chore(pages): add webp image in not found page

--- frontend/trackle/src/pages/NotFound.tsx
+++ frontend/trackle/src/pages/NotFound.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Link } from '@tanstack/react-router';
-
+import creatureImg from './../assets/sadCreature.webp';
 const NotFound:React.FC = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-yellow-50 p-8 text-center">
-      <h1 className="text-5xl font-bold text-yellow-600 mb-4">404 - Not Found</h1>
+      <h1 className="text-3xl lg:text-5xl font-bold text-yellow-600 mb-4">404 - Not Found</h1>
       <img
-        src="https://i.pinimg.com/originals/1e/34/14/1e341474eaee74b7fca75f477e26a8c4.png"
-        alt="Teary Pikachu"
-        className="w-64 h-auto mb-6 drop-shadow-lg"
+        src={creatureImg}
+        alt="Teary eyed creature"
+        className="w-48 lg:w-64 h-auto mb-6 drop-shadow-lg"
       />
       <p className="text-lg text-gray-700 mb-6">
        The page you are looking for does not exist or has been moved.
