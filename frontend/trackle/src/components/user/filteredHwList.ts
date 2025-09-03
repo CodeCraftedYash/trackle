7fec3c06c6915d3d20ab84096f08b6845eed7bfd
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-09-03 13:51:58 +0530

    feat(component): add a util file to filter empty homeworks

---
+++ frontend/trackle/src/components/user/filteredHwList.ts
@@ -0,0 +1,33 @@
+import { useAuthStore } from "../../store/authStore";
+
+const todayDate = new Date().getDate() - 1;
+const currentMonth = new Date().toLocaleString("default", { month: "long" });
+
+export const filterHw = () => {
+  const homework = useAuthStore.getState().user?.homework;
+  if (!homework) {
+    throw Error("No homework data available");
+  }
+  const todayHw = {
+    index: todayDate,
+    hw: homework[todayDate + 1],
+    month: currentMonth,
+  };
+  const previousMonth = new Date(
+    new Date().getFullYear(),
+    new Date().getMonth() - 1
+  ).toLocaleString("default", { month: "long" });
+  const after = homework
+    .slice(todayDate + 1)
+    .map((hw, index) => ({
+      index: todayDate + 1 + index,
+      hw,
+      month: previousMonth,
+    }))
+    .filter((item) => item.hw !== "");
+  const before = homework
+    .slice(0, todayDate)
+    .map((hw, index) => ({ index: index, hw, month: currentMonth }))
+    .filter((item) => item.hw !== "");
+  return [...before, todayHw, ...after];
+};
