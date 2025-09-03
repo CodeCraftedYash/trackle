2a92812bb326d02760e37344f568dcd9079eaaf7
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-09-03 13:53:17 +0530

    feat(component): add sliding animation using motion library

--- frontend/trackle/src/components/user/HomeWork.tsx
+++ frontend/trackle/src/components/user/HomeWork.tsx
@@ -1,31 +1,44 @@
-import { forwardRef } from 'react'
+import React from 'react';
+import { AnimatePresence, motion } from 'motion/react';
+import { slideVariant } from '../../variants/slideVariant';
 
 type HomeWorkProps = {
   todayHW?: string;
+  title?: string
+  direction: "next" | "prev";
+  hwIndex: number;
 }
 
-const HomeWork = forwardRef<HTMLDivElement, HomeWorkProps>(
-  ({ todayHW }, ref) => {
-    return (
-      <div
-        className='flex flex-col items-center mx-auto text-[var(--color-text)]'
-        ref={ref}
+const HomeWork: React.FC<HomeWorkProps> = (({ todayHW, title, direction, hwIndex }) => {
+  return (
+    <div
+      className='flex flex-col items-center mx-auto text-[var(--color-text)] w-full'
+    >
+      <h1
+        style={{ fontSize: "var(--font-size-base)" }}
+        className='bg-[var(--color-accent)] font-bold text-nowrap p-2 w-full text-center whitespace-nowrap rounded-lg '
       >
-        <h1
-          style={{ fontSize: "var(--font-size-base)" }}
-          className='bg-[var(--color-accent)] font-bold text-nowrap p-2 w-full text-center whitespace-nowrap rounded-lg'
-        >
-          Today's Home Work
-        </h1>
-        <p
-          className='mt-4 border-2 p-2 rounded-lg'
-          style={{ fontSize: "var(--font-size-base)" }}
-        >
-          {todayHW ? todayHW : "No homework assigned for today"}
-        </p>
+        {title ? title : "Today's Home Work"}
+      </h1>
+      <div className=' mt-4 border-2 p-2 rounded-lg overflow-hidden'>
+        <AnimatePresence mode='wait' custom={direction}>
+          <motion.p
+            className=' min-h-20 w-full'
+            style={{ fontSize: "var(--font-size-base)" }}
+            key={hwIndex}
+            variants={slideVariant}
+            animate="animate"
+            initial="initial"
+            exit="exit"
+            custom={direction}
+          >
+            {todayHW ? todayHW : "No homework assigned for today"}
+          </motion.p>
+        </AnimatePresence>
       </div>
-    )
-  }
+    </div>
+  )
+}
 )
 
 export default HomeWork
