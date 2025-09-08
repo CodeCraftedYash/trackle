3e3d37fce6d167531a1be8cc45d1f0fa256025d4
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-09-08 13:44:00 +0530

    style(components): improve dilog box ui

--- frontend/trackle/src/components/dialogBox/DialogBox.tsx
+++ frontend/trackle/src/components/dialogBox/DialogBox.tsx
@@ -17,11 +17,11 @@ const DialogBox: React.FC = () => {
   if (!isOpen) return null;
 
   return (
-    <div className='absolute top-[50%] left-[50%] -translate-[50%] aspect-video w-[20rem] lg:w-[40rem] bg-white z-50 border-4 border-[var(--color-border)] transition-all duration-300 ease-in-out flex-co items-center justify-center rounded-xl text-center text-[var(--color-background)] '>
-      <h1 className=' w-full border-b-2 border-[var(--color-border)] py-4 font-bold' style={{fontSize:"var(--font-size-large)"}}>
+    <div className='absolute top-[40%] left-[50%] -translate-[50%] aspect-video w-[20rem] lg:w-[40rem] bg-white z-50 border-4 border-[var(--color-border)] transition-all duration-300 ease-in-out flex-co items-center justify-center rounded-xl text-center'>
+      <h1 className=' w-full border-b-2 border-[var(--color-border)] py-4 font-bold text-[var(--color-text-heading)] bg-[var(--color-surface)]' style={{ fontSize: "var(--font-size-semi-large)" }}>
         {title}
       </h1>
-      <h2 className='text-lg mt-4 font-semibold'style={{fontSize:"var(--font-size-base)"}}>{message}</h2>
+      <h2 className='mt-4 font-semibold text-[var(--color-text)]' style={{ fontSize: "var(--font-size-base)" }}>{message}</h2>
       <div className='absolute bottom-1 w-full border-2 overflow-hidden h-[1rem]'>
         <motion.div
           className='absolute h-[1rem] bg-green-500'
