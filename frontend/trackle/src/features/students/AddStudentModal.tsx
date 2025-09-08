92ff7fa51f692b657d0fc40d39a3e8a3186190db
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-09-08 14:01:17 +0530

    style(features): imrove ui of add student modal feature

--- frontend/trackle/src/features/students/AddStudentModal.tsx
+++ frontend/trackle/src/features/students/AddStudentModal.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState,type ChangeEvent,type FormEvent } from 'react';
+import React, { useRef, useState, type ChangeEvent, type FormEvent } from 'react';
 import type { AddStudentData, AddStudentModalProps } from '../../types/AddStudentDataType';
 import { FaRegEye, FaRegEyeSlash, FaRegFolderOpen } from "react-icons/fa";
 import { motion } from 'motion/react';
@@ -16,32 +16,32 @@ const AddStudentModal: React.FC<AddStudentModalProps> = ({ onClose, onSubmit }) 
   });
   const [viewPassword, setViewPassword] = useState(false);
   const elementRef = useRef<HTMLDivElement>(null);
-  useClickOutside(elementRef,onClose);
-  
+  useClickOutside(elementRef, onClose);
+
   const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
-  const target = e.target;
-  if (target instanceof HTMLInputElement) {
-    if (target.type === 'file') {
-      setForm((prev) => ({ ...prev, [target.name]: target.files ? target.files[0] : null }));
-    } else {
+    const target = e.target;
+    if (target instanceof HTMLInputElement) {
+      if (target.type === 'file') {
+        setForm((prev) => ({ ...prev, [target.name]: target.files ? target.files[0] : null }));
+      } else {
+        setForm((prev) => ({ ...prev, [target.name]: target.value }));
+      }
+    } else if (target instanceof HTMLSelectElement) {
       setForm((prev) => ({ ...prev, [target.name]: target.value }));
     }
-  } else if (target instanceof HTMLSelectElement) {
-    setForm((prev) => ({ ...prev, [target.name]: target.value }));
-  }
-};
+  };
 
   const handleSubmit = (e: FormEvent) => {
-  const formData = new FormData();
-  formData.append("name", form.name);
-  formData.append("gender", form.gender);
-  formData.append("password", form.password);
-  formData.append("mobileNumber", form.mobileNumber);
-  formData.append("standard", form.standard);
+    const formData = new FormData();
+    formData.append("name", form.name);
+    formData.append("gender", form.gender);
+    formData.append("password", form.password);
+    formData.append("mobileNumber", form.mobileNumber);
+    formData.append("standard", form.standard);
 
-  if (form.picture) {
-    formData.append("picture", form.picture);
-  }
+    if (form.picture) {
+      formData.append("picture", form.picture);
+    }
 
     e.preventDefault();
     onSubmit(formData);
@@ -50,12 +50,12 @@ const AddStudentModal: React.FC<AddStudentModalProps> = ({ onClose, onSubmit }) 
   const baseClass = "w-full border p-2 rounded bg-white/90 text-black placeholder:text-black/80 "
   return (
     <motion.div className="fixed w-full inset-0 bg-transparent flex items-center justify-center z-50 top-[50%] left-[50%] -translate-[50%]"
-    style={{ fontSize: "var(--font-size-base)" }}
-    variants={fadeIn}
-    animate="animate" initial="initial" exit="exit"
+      style={{ fontSize: "var(--font-size-base)" }}
+      variants={fadeIn}
+      animate="animate" initial="initial" exit="exit"
     >
       <div className="bg-[var(--color-surface)] p-6 rounded-lg w-[90%] lg:w-full max-w-md shadow-lg" ref={elementRef}>
-        <h2 className=" font-bold mb-4" style={{fontSize:"var(--font-size-large)"}}>Add Student</h2>
+        <h2 className=" font-bold mb-4" style={{ fontSize: "var(--font-size-semi-large)" }}>Add Student</h2>
         <form onSubmit={handleSubmit} className="space-y-4">
 
           <input
@@ -103,23 +103,23 @@ const AddStudentModal: React.FC<AddStudentModalProps> = ({ onClose, onSubmit }) 
             </label>
           </div>
           <div className='flex-col flex-nowrap gap-4 relative '>
-          <input
-            type={viewPassword ? "text" : "password"}
-            name="password"
-            value={form.password}
-            onChange={handleChange}
-            placeholder="Password"
-            required
-            className={`${baseClass}`}
-          />
-          <button
-            type="button"
-            onClick={() => setViewPassword(!viewPassword)}
-            className="absolute right-2 top-3 hover:cursor-pointer hover:scale-125 text-black "
-          >
+            <input
+              type={viewPassword ? "text" : "password"}
+              name="password"
+              value={form.password}
+              onChange={handleChange}
+              placeholder="Password"
+              required
+              className={`${baseClass}`}
+            />
+            <button
+              type="button"
+              onClick={() => setViewPassword(!viewPassword)}
+              className="absolute right-2 top-3 hover:cursor-pointer hover:scale-125 text-black "
+            >
               {viewPassword ? <FaRegEye /> : <FaRegEyeSlash />}
-          </button>
-        </div>
+            </button>
+          </div>
           <input
             type="text"
             name="mobileNumber"
@@ -130,14 +130,14 @@ const AddStudentModal: React.FC<AddStudentModalProps> = ({ onClose, onSubmit }) 
             className={`${baseClass}`}
           />
           <label className='flex flex-nowrap items-center  bg-white/90 relative'>
-          <input
-            type="file"
-            name="picture"
-            accept="image/*"
-            onChange={handleChange}
-            className="w-full hover:cursor-pointer text-black/90 p-2"
-          />
-          <h1 className='text-xl text-yellow-500 absolute right-4'><FaRegFolderOpen /></h1>
+            <input
+              type="file"
+              name="picture"
+              accept="image/*"
+              onChange={handleChange}
+              className="w-full hover:cursor-pointer text-black/90 p-2"
+            />
+            <h1 className='text-xl text-yellow-500 absolute right-4'><FaRegFolderOpen /></h1>
           </label>
           <input
             type="text"
