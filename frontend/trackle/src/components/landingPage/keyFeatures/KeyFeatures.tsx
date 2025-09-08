54420d68d17c608a48a6aff17c00c109b1893ffd
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-09-08 13:51:02 +0530

    style(components): improve ui of key features  section in landing >> page

--- frontend/trackle/src/components/landingPage/keyFeatures/KeyFeatures.tsx
+++ frontend/trackle/src/components/landingPage/keyFeatures/KeyFeatures.tsx
@@ -7,15 +7,23 @@ const KeyFeatures: React.FC = () => {
             initial={{ opacity: 0, y: 30 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.6, delay: 0.4 }}
-            className=' w-full min-h-40 bg-[var(--color-background)] text-center border-t-2 pt-4 lg:pt-12 border-white/20'>
-            <h1 className='text-lg font-bold lg:text-2xl text-[var(--color-text)] mb-1' style={{fontSize:"var(--font-size-large)"}}>Key Features</h1>
-            <h2 className='text-sm lg:text-sm w-[95%] mx-auto' style={{fontSize:"var(--font-size-base)"}}>Trackle offers a comprehensive suite of tools to enhance your teaching experience.</h2>
-            <div className='w-full flex  justify-between gap-2 mt-4 flex-col md:flex-row md:mt-10'>
+            className=' w-full min-h-40  text-center pt-4 '>
+            <div className=' bg-[var(--color-surface)] py-4 rounded-xl mb-8 w-[80%] mx-auto'>
+                <h1 className='text-lg font-bold text-[var(--color-text-heading)] mb-1  ' style={{ fontSize: "var(--font-size-large)" }}>Key Features</h1>
+            </div>
+            <h2 className='text-[var(--color-text)] mx-auto w-[80%] mb-8' style={{ fontSize: "var(--font-size-small)" }}>Trackle offers a comprehensive suite of tools to enhance your teaching experience.</h2>
+            <div className='w-full flex  justify-between gap-2 mt-4 flex-col md:mt-10'>
                 {
                     keyFeaturesContent.map((item, index) => (
-                        <div key={index} className=' border-2 border-[var(--color-border)] w-[90%] mx-auto  min-h-[10rem] p-3 lg:p-12'>
-                            <h1 className='font-medium mb-2' style={{fontSize:"var(--font-size-semi-large)"}}>{item.title}</h1>
-                            <p className='font-light' style={{fontSize:"var(--font-size-base)"}}>{item.body}</p>
+                        <div className='flex w-full mb-12 lg:justify-between lg:items-start'>
+                            <div key={index} className='flex flex-col  lg:mt-8 w-full sm:mx-auto'>
+                                <h1 className='text-[var(--color-text-heading)] bg-[var(--color-surface)] rounded-xl font-thin w-[60%] px-2 py-4 mx-auto text-wrap ' style={{ fontSize: "var(--font-size-semi-large)" }}>{item.title}</h1>
+
+                                <img src={item.imgSrc} alt={item.title} loading="lazy" className='lg:hidden w-[55%] max-w-[56%] sm:w-[43%]  md:w-[25%] mx-auto my-4 lg:my-0' />
+
+                                <p className='font-light lg:mt-12 sm:w-[80%] sm:mx-auto md:w-[70%] lg:w-[100%]' style={{ fontSize: "var(--font-size-base)" }}>{item.body}</p>
+                            </div>
+                            <img src={item.imgSrc} alt={item.title} className='w-[25%] hidden lg:block' />
                         </div>
                     ))
                 }
--- frontend/trackle/src/components/landingPage/keyFeatures/keyFeaturesContents.ts
+++ frontend/trackle/src/components/landingPage/keyFeatures/keyFeaturesContents.ts
@@ -1,7 +1,22 @@
 import type { KeyFeaturesType } from "../../../types/kyeFeaturesType";
+import socialMediaImg from "../../../assets/social-media.webp";
+import analyticsImage from "../../../assets/presentation.webp";
+import performanceImage from "../../../assets/productivity.webp";
 
-export const keyFeaturesContent:KeyFeaturesType[] = [
-    {title:"Student Management",body:"Easily organize and manage your student roster, including contact information and individual progress."},
-    {title:"Interactive ",body:"create engaging and productive learning environment and collaborative activites"},
-    {title:"Performance Tracking",body:"Monitor student performance with detailed analytics and reports, allowing you to identify areas for improvement and tailor your instruction accordingly."},
-]
+export const keyFeaturesContent: KeyFeaturesType[] = [
+  {
+    title: "Student Management",
+    body: "Easily organize and manage your student roster, including contact information and individual progress.",
+    imgSrc: analyticsImage,
+  },
+  {
+    title: "Interactive ",
+    body: "create engaging and productive learning environment and collaborative activites",
+    imgSrc: socialMediaImg,
+  },
+  {
+    title: "Performance Tracking",
+    body: "Monitor student performance with detailed analytics and reports, allowing you to identify areas for improvement and tailor your instruction accordingly.",
+    imgSrc: performanceImage,
+  },
+];
