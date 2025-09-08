cc9ac3cd5c702599c42196e79ebaa87ea5160d23
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-09-08 13:47:07 +0530

    style(components): improve header ui

--- frontend/trackle/src/components/header/Header.tsx
+++ frontend/trackle/src/components/header/Header.tsx
@@ -11,46 +11,48 @@ const Header: React.FC = () => {
   const elementRef = React.useRef<HTMLDivElement>(null);
   useClickOutside(elementRef, () => setOpen(false));
   return (
-    <header ref={elementRef} className='w-full h-fit py-2 bg-[var(--color-surface)] flex flex-nowrap items-center font-semibold px-2 border-b-4 border-[var(--color-border)] justify-between'
+    <header ref={elementRef} className='w-full h-fit py-2 bg-[var(--color-surface)] flex flex-nowrap items-center font-semibold px-2 justify-between'
       style={{
         fontSize: 'var(--font-size-base)',
+        position:'relative',
+         boxShadow: "0 4px 10px rgba(0,0,0,0.15)",
       }}
     >
       <Logo />
       <div className='hidden lg:block'><Navbar /></div>
-      <div className='md:hidden relative flex items-center justify-center mr-4 -translate-y-2'>
+      <div className='lg:hidden relative flex items-center justify-center mr-4 -translate-y-2'>
         {<AnimatePresence>
           {!open && <motion.div
             key={`${open}`}
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
-          ><GiHamburgerMenu style={{ fontSize: "var(--font-size-large)" }} className='font-bold absolute text-[var(--color-text)] ' onClick={() => setOpen(true)} /></motion.div>}
+          ><GiHamburgerMenu style={{ fontSize: "var(--font-size-large)" }} className='font-bold absolute text-[var(--color-text-heading)] hover:cursor-pointer' onClick={() => setOpen(true)} /></motion.div>}
 
           {open && <motion.div
             key={`${open}`}
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
-          ><IoClose style={{ fontSize: "calc(var(--font-size-large) + 4px)" }} className='font-bold absolute text-[var(--color-text)] pr-0.5 pb-0.5' onClick={() => setOpen(false)} /></motion.div>}
+          ><IoClose style={{ fontSize: "calc(var(--font-size-large) + 4px)" }} className='font-bold absolute text-[var(--color-text-heading)] pr-0.5 pb-0.5' onClick={() => setOpen(false)} /></motion.div>}
 
         </AnimatePresence>}
       </div>
       <AnimatePresence>
-      {open && <div className='absolute lg:hidden right-0 top-14 pl-2 z-[100] overflow-x-hidden'><motion.div
-      initial={{x:"100%",opacity:0}}
-      animate={{x:"0%",opacity:1}}
-      exit={{opacity:0 ,x:"100%"}}
-      transition={{
-        duration:0.2,
-        ease:"easeInOut",
+        {open && <div className='absolute lg:hidden right-0 top-14 pl-2 z-[100] overflow-x-hidden'><motion.div
+          initial={{ x: "100%", opacity: 0 }}
+          animate={{ x: "0%", opacity: 1 }}
+          exit={{ opacity: 0, x: "100%" }}
+          transition={{
+            duration: 0.2,
+            ease: "easeInOut",
 
-      }}
-      className='bg-black/50  p-2 pr-6 border-r-0 rounded-l-xl border-2 border-[var(--color-text)] '
-      >
-            <Navbar setOpen={setOpen}/>
-      </motion.div>
-      </div>}
+          }}
+          className='bg-[var(--color-surface)]/80  p-2 pr-6 border-r-0 rounded-l-xl border-2 border-[var(--color-text-heading)] '
+        >
+          <Navbar setOpen={setOpen} />
+        </motion.div>
+        </div>}
       </AnimatePresence>
     </header>
   )
