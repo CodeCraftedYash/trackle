e6d1b167d4e6db22a1373338853d7d0093c213bd
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-09-08 13:55:37 +0530

    style(components): improve ui of table component

--- frontend/trackle/src/components/table/Table.tsx
+++ frontend/trackle/src/components/table/Table.tsx
@@ -18,18 +18,18 @@ const Table: React.FC = () => {
       <td className='p-2 border-b-2 text-left  font-bold text-[var(--color-text)] border-[var(--color-surface)]' style={{ fontSize: "var(--font-size-base)" }}>{item.mobileNumber}</td>
       <td className='p-2 border-b-2 text-left  font-bold text-[var(--color-text)] border-[var(--color-surface)]' style={{ fontSize: "var(--font-size-base)" }}>{item.fees}</td>
       <td className='p-2 border-b-2 text-left  font-bold text-[var(--color-text)] border-[var(--color-surface)]' style={{ fontSize: "var(--font-size-base)" }}>{item.isFeesPaid ? "yes" : "no"}</td>
-      <td className='p-2 min-w-[12rem] max-w-[13rem] text-left font-bold text-[var(--color-text)] truncate border-b-2 border-[var(--color-surface)] text-wrap' style={{ fontSize: "var(--font-size-base)" }}>{item.homework?.[today] ?? "No homework"}</td>
+      <td className='p-2 min-w-[12rem] max-w-[13rem] text-left font-bold text-[var(--color-text)] truncate border-b-2 border-[var(--color-surface)] text-wrap' style={{ fontSize: "var(--font-size-base)" }}>{item.homework?.[today] != "" ? item.homework?.[today] : "No homework"}</td>
     </tr>)
   })
   return (
-    <div className='w-full rounded-xl'>
-      <h1 className='font-bold w-full text-center mx-auto bg-[var(--color-accent)] text-[var(--color-text)] px-10 py-2 rounded-xl mb-8 border-[var(--color-surface)] border-2'
+    <div className='w-full rounded-xl mt-12'>
+      <h1 className='font-bold w-full text-center mx-auto bg-[var(--color-surface)] text-[var(--color-text-heading)] px-10 py-2 rounded-xl mb-8 border-[var(--color-surface)] border-2'
         style={{ fontSize: "var(--font-size-semi-large)" }}
       >Student Data</h1>
-      <div className='w-full overflow-x-scroll border-2 rounded-xl border-[var(--color-surface)]' style={{ scrollbarWidth: 'none', scrollBehavior: 'smooth' }}>
+      <div className='w-full overflow-x-scroll border-2 rounded-xl border-[var(--color-text)]' style={{ scrollbarWidth: 'none', scrollBehavior: 'smooth' }}>
         <table className=' w-full '>
           <thead>
-            <tr className=' rounded-xl border-[var(--color-surface)]'>
+            <tr className=' rounded-xl border-[var(--color-text)]'>
               {tableHeadings}
             </tr>
           </thead>
