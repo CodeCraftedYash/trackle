68df734bc7fd9e4dd9c62a01707115430e04005e
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-09-08 14:09:32 +0530

    feat(page): add index file for centralize exports

---
+++ frontend/trackle/src/pages/about/index.ts
@@ -0,0 +1,9 @@
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { BiLogoGmail } from "react-icons/bi";
+import  read  from '../../assets/read.webp';
+import sad from '../../assets/sad.webp';
+import angry from '../../assets/angry.webp';
+import wow from '../../assets/wow.webp';
+
+export {FaGithub, FaLinkedin, BiLogoGmail,read,sad,angry,wow};
