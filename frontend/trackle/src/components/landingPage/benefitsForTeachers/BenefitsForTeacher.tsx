97e720340ac2266f693ab418707fe369ce73a749
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-09-08 13:49:46 +0530

    style(components): improve ui of benefits for teacher section in landing page

--- frontend/trackle/src/components/landingPage/benefitsForTeachers/BenefitsForTeacher.tsx
+++ frontend/trackle/src/components/landingPage/benefitsForTeachers/BenefitsForTeacher.tsx
@@ -3,34 +3,34 @@ import { motion } from 'motion/react';
 import { benefitContent } from './benefitContent';
 
 const BenefitsForTeacher: React.FC = () => {
-  return (
+    return (
         <motion.div
             initial={{ opacity: 0, y: 30 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.6, delay: 0.4 }}
             className=' w-full min-h-40  text-center pt-4 '>
             <div className=' bg-[var(--color-surface)] py-4 rounded-xl mb-8 w-[80%] mx-auto'>
-            <h1 className='text-lg font-bold text-[var(--color-text-heading)] mb-1 px-2 ' style={{fontSize:"var(--font-size-large)"}}>Benefits For Teacher</h1>
+                <h1 className='text-lg font-bold text-[var(--color-text-heading)] mb-1 px-2 ' style={{ fontSize: "var(--font-size-large)" }}>Benefits For Teacher</h1>
             </div>
-            <h2 className='text-[var(--color-text)] mx-auto w-[80%] mb-8' style={{fontSize:"var(--font-size-small)"}}>Trackle is designed to make your life easier and more efficient.</h2>
+            <h2 className='text-[var(--color-text)] mx-auto w-[80%] mb-8' style={{ fontSize: "var(--font-size-small)" }}>Trackle is designed to make your life easier and more efficient.</h2>
             <div className='w-full flex  justify-between gap-2 mt-4 flex-col md:mt-10'>
                 {
                     benefitContent.map((item, index) => (
                         <div className='flex w-full mb-12 lg:justify-between lg:items-start'>
-                        <div key={index} className='flex flex-col  lg:mt-8 w-full sm:mx-auto'>
-                            <h1 className='text-[var(--color-text-heading)] bg-[var(--color-surface)] rounded-xl font-thin w-[60%] px-2 py-4 mx-auto text-wrap' style={{fontSize:"var(--font-size-semi-large)"}}>{item.heading}</h1>
+                            <div key={index} className='flex flex-col  lg:mt-8 w-full sm:mx-auto'>
+                                <h1 className='text-[var(--color-text-heading)] bg-[var(--color-surface)] rounded-xl font-thin w-[60%] px-2 py-4 mx-auto text-wrap' style={{ fontSize: "var(--font-size-semi-large)" }}>{item.heading}</h1>
 
-                            <img src={item.src} alt={item.alt} loading="lazy" className='lg:hidden w-[55%] max-w-[56%] sm:w-[43%]  md:w-[25%] mx-auto my-4 lg:my-0' />
+                                <img src={item.src} alt={item.alt} loading="lazy" className='lg:hidden w-[55%] max-w-[56%] sm:w-[43%]  md:w-[25%] mx-auto my-4 lg:my-0' />
 
-                            <p className='font-light lg:mt-12 sm:w-[80%] sm:mx-auto md:w-[70%] lg:w-[100%]' style={{fontSize:"var(--font-size-base)"}}>{item.body}</p>
-                        </div>
-                        <img src={item.src} alt={item.alt} className='w-[25%] hidden lg:block' />
+                                <p className='font-light lg:mt-12 sm:w-[80%] sm:mx-auto md:w-[70%] lg:w-[100%]' style={{ fontSize: "var(--font-size-base)" }}>{item.body}</p>
+                            </div>
+                            <img src={item.src} alt={item.alt} className='w-[25%] hidden lg:block' />
                         </div>
                     ))
                 }
             </div>
         </motion.div>
-  )
+    )
 }
 
 export default BenefitsForTeacher
--- frontend/trackle/src/components/landingPage/benefitsForTeachers/benefitContent.ts
+++ frontend/trackle/src/components/landingPage/benefitsForTeachers/benefitContent.ts
@@ -1,25 +1,25 @@
 import type { benefitContentType } from "../../../types/benefitContentType";
-import img1 from '../../../assets/time.webp';
-import img2 from '../../../assets/leaves.webp';
-import img3 from '../../../assets/exam-results.webp';
+import img1 from "../../../assets/time.webp";
+import img2 from "../../../assets/leaves.webp";
+import img3 from "../../../assets/exam-results.webp";
 
-export const benefitContent:benefitContentType[] = [
-    {
-        src:img1,
-        alt:"Save Time",
-        heading:"Save Time",
-        body:"Automate administrative tasks, such as attendance tracking and grading, freeing up valuable time for lesson planning and student interaction.",
-    },
-    {
-        src:img2,
-        alt:"Increase Engagement",
-        heading:"Increase Engagement",
-        body:"Create dynamic and interactive lessons that captivate students' attention and foster a more engaging learning environment.",
-    },
-    {
-        src:img3,
-        alt:"Improve Outcomes",
-        heading:"Improve Outcomes",
-        body:"Gain insights into student performance and tailor your instruction to meet individual needs, leading to improved learning outcomes.",
-    },
-]
+export const benefitContent: benefitContentType[] = [
+  {
+    src: img1,
+    alt: "Save Time",
+    heading: "Save Time",
+    body: "Automate administrative tasks, such as attendance tracking and grading, freeing up valuable time for lesson planning and student interaction.",
+  },
+  {
+    src: img2,
+    alt: "Increase Engagement",
+    heading: "Increase Engagement",
+    body: "Create dynamic and interactive lessons that captivate students' attention and foster a more engaging learning environment.",
+  },
+  {
+    src: img3,
+    alt: "Improve Outcomes",
+    heading: "Improve Outcomes",
+    body: "Gain insights into student performance and tailor your instruction to meet individual needs, leading to improved learning outcomes.",
+  },
+];
