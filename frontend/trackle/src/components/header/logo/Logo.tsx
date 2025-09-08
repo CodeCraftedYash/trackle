8e4c9b41e70ddc341efce19b7ad8671b4433d029
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-09-08 13:45:17 +0530

    style(components): improve logo ui

--- frontend/trackle/src/components/header/logo/Logo.tsx
+++ frontend/trackle/src/components/header/logo/Logo.tsx
@@ -1,17 +1,17 @@
 import { useNavigate } from '@tanstack/react-router';
 import React from 'react'
 
-const Logo:React.FC = () => {
+const Logo: React.FC = () => {
   const navigate = useNavigate();
   const handleClick = () => {
     navigate({ to: '/' });
   }
   return (
     <div className='box-border' onClick={handleClick}>
-      <h2 className='text-[var(--color-text)]'
-      style={{
-        fontSize: 'var(--font-size-large)'
-      }}>Trackle</h2>
+      <h2 className='text-[var(--color-text-heading)] font-black italic hover:cursor-pointer'
+        style={{
+          fontSize: 'var(--font-size-large)'
+        }}>Trackle</h2>
     </div>
   )
 }
