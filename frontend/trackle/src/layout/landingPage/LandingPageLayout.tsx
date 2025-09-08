9726b90c663acd7d6ce282c8354775b6f316c73a
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-09-08 14:06:01 +0530

    style(layout): imrove ui of landing page layout

--- frontend/trackle/src/layout/landingPage/LandingPageLayout.tsx
+++ frontend/trackle/src/layout/landingPage/LandingPageLayout.tsx
@@ -4,16 +4,17 @@ import KeyFeatures from '../../components/landingPage/keyFeatures/KeyFeatures'
 import BenefitsForTeacher from '../../components/landingPage/benefitsForTeachers/BenefitsForTeacher'
 import ReadyToTransform from '../../components/landingPage/ReadyToTransform/ReadyToTransform'
 import { useAuthStore } from '../../store/authStore'
-const LandingPageLayout:React.FC = () => {
+
+const LandingPageLayout: React.FC = () => {
   const token = useAuthStore(state => state.token);
   return (
     <div className='w-full min-h-[90dvh] bg-[var(--color-background)] text-[var(--color-text)] box-border py-4'>
-        <main className='w-[90%] lg:w-[65%]  min-h-20 mx-auto lg:mt-8 flex flex-col gap-10 lg:gap-32'>
-           <Hero />
-           <KeyFeatures />
-           <BenefitsForTeacher />
-           {!token&&<ReadyToTransform />}
-        </main>
+      <main className='w-[90%] lg:w-[65%]  min-h-20 mx-auto lg:mt-8 flex flex-col gap-10 lg:gap-20'>
+        <Hero />
+        <KeyFeatures />
+        <BenefitsForTeacher />
+        {!token && <ReadyToTransform />}
+      </main>
     </div>
   )
 }
